49797cbe87853e858a9077b9f5d997a04628aecd
Yunarian <167535928+Yunarian@users.noreply.github.com>
2024-10-17 13:03:55 +0000

    gah

--- src/main/java/com/example/project/LinearCalculator.java
+++ src/main/java/com/example/project/LinearCalculator.java
@@ -1,49 +1,69 @@
 package com.example.project;
+
 public class LinearCalculator{
     //INSTANCE VARIABLES 
-    //4 INTEGER variables (name them: x1,x2,y1,y2) 
-
+    //4 INTEGER variables (name them: x1,x2,y1,y2)
+    private int x1; 
+    private int x2; 
+    private int y1; 
+    private int y2; 
 
 
     //CONSTRUCTOR
     //1 constructor with 2 String parameters. Each parameter represents a coordinate. 
     //For example, "(1,2)" and "(3,4)" would be two parameter values 
     //You will have to parse the string into 4 integers, representing the 2 points.
-    public LinearCalculator(){ // <--add 2 string parameters to this constructor
-     
+    //for x-values: the substring between the "(" and the "," is taken & converted to an int
+    //for y-values: the substring between the "," and the ")" is taken & converted to an int
+    public LinearCalculator(String point1, String point2){ // <--add 2 string parameters to this constructor
+        x1 = Integer.valueOf(point1.substring(1, point1.indexOf(",")));
+        y1 = Integer.valueOf(point1.substring(point1.indexOf(",") + 1, point1.length() - 1));
+        x2 = Integer.valueOf(point2.substring(1, point2.indexOf(",")));
+        y2 = Integer.valueOf(point2.substring(point2.indexOf(",") + 1, point2.length() - 1));
     }
-
 
 
     //METHODS
     //getters and setters for the 4 instance variables (8 methods total) 
-    public int getX1(){return 0;}
-    public int getY1(){return 0;}
-    public int getX2(){return 0;}
-    public int getY2(){return 0;}
-    public void setX1(){}
-    public void setY1(){}
-    public void setX2(){}
-    public void setY2(){}
+    public int getX1(){return x1;}
+    public int getY1(){return y1;}
+    public int getX2(){return x2;}
+    public int getY2(){return y2;}
+    public void setX1(int newX1){x1 = newX1;}
+    public void setY1(int newY1){y1 = newY1;}
+    public void setX2(int newX2){x2 = newX2;}
+    public void setY2(int newY2){y2 = newY2;}
 
 
     //distance() -> returns a double. 
     //calculates the distance between the two points to the nearest HUNDREDTH and returns the value.
+    //takes the difference in x and y values, squares then, adds them together, then takes the square root of that result
     public double distance(){
-        return 0.0;
-
-    //yInt() -> returns a double.
-    //calculates the y intercept of the equation and returns the value to the nearest HUNDREDTH
-    //if y-int if undefined, should return -999.99
-    public double yInt(){
-        return 0.0;
+        double distance = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+        return roundedToHundredth(distance);
     }
 
     //slope() -> returns a double. 
     //calculates the slope of the equations and returns the value to the nearest HUNDREDTH
     //if slope is undefined, should return -999.99
     public double slope(){
-        return 0.0;
+        //to see if the slope is undefined
+        if (x1 == x2) {
+            return -999.99;
+        }
+        return roundedToHundredth( (double) (y2 - y1)/(x2 - x1));
+    }
+
+    //yInt() -> returns a double.
+    //calculates the y intercept of the equation and returns the value to the nearest HUNDREDTH
+    //if y-int if undefined, should return -999.99
+    public double yInt(){
+        double slope = slope();
+        //to see if the y-intercept is undefined
+        if (x1 == x2) {
+            return -999.99;
+        }
+        return roundedToHundredth(y1 - (slope * x1));
     }
 
     //equations() -> returns a String.
@@ -51,26 +71,42 @@ public class LinearCalculator{
     //if the equation has no slope, the equation should return -> "undefined"
     //HINT: You may need other custom methods to decrease the amount of code in the equations() method
     public String equation(){
-        return "";
+        double slope = slope();
+        double yInt = yInt();
+        //checks to see if the equation is undefined
+        if (x1 == x2) {
+            return "undefined";
+        }
+
+        if (yInt != 0.0 && slope != 0.0) {
+            if (yInt >= 0) {return "y=" + slope + "x+" + yInt;}
+
+            return "y=" + slope + "x" + yInt;
+                
+        } else if (yInt == 0.0) {
+            return "y=" + slope + "x";
+
+        }
+        return "y=" + yInt;
     }
 
 
     //roundedToHundredth(double x)-> returns double
     //calculates the input to the nearest hundredth and returns that value
     public double roundedToHundredth(double x){
-        return 0.0;
+        return Math.round(x * 100) / 100.0;
     }
 
     //printInfo() -> returns a string of information
     //this method is tested but you can also call it in your main method if gradle tests are 
     //not working. 
     public String printInfo(){
-        String str = "The two points are: (" + /*insert var here*/ + "," +/*insert var here*/  + ")";
-        str += " and " + "(" + /*insert var here*/ + "," + /*insert var here*/ + ")";
-        str += "\nThe equation of the line between these points is: " ;
-        str += "\nThe slope of this line is: ";
-        str += "\nThe y-intercept of the line is: ";
-        str += "\nThe distance between the two points is: ";
+        String str = "The two points are: (" + x1 +  "," + y1  + ")";
+        str += " and " + "(" + x2 + "," + y2 + ")";
+        str += "\nThe equation of the line between these points is: " + equation();
+        str += "\nThe slope of this line is: " + slope();
+        str += "\nThe y-intercept of the line is: " + yInt();
+        str += "\nThe distance between the two points is: " + distance();
  
         return str;
     }
---
+++ src/main/java/com/example/project/LinearCalculatorRunner.java
@@ -0,0 +1,9 @@
+package com.example.project;
+
+public class LinearCalculatorRunner {
+    public static void main(String[] args) {
+        LinearCalculator c = new LinearCalculator("(10,5)","(6,-1)");
+        System.out.println(c.printInfo());
+    }
+    
+}
